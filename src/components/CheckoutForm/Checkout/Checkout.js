5cc4e2a41f44aa51ca7d3a454a60773736a462ab
quocpham97 <phamxuandinhquoc@example.com>
2021-04-10 11:16:37 +0700

    review cart

--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from "@material-ui/core";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button
+} from "@material-ui/core";
 
 import { commerce } from "../../../lib/commerce";
 import useStyles from "./styles";
@@ -12,6 +21,7 @@ function Checkout({ cart }) {
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
 
   useEffect(async () => {
     const generateToken = async () => {
@@ -25,14 +35,27 @@ function Checkout({ cart }) {
     generateToken();
   }, [cart]);
 
-  const Form = () => (activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} /> : <PaymentForm />);
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+    nextStep();
+  };
+
+  const Form = () =>
+    activeStep === 0 ? (
+      <AddressForm checkoutToken={checkoutToken} next={next} />
+    ) : (
+      <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+    );
   const Confirmation = () => <div>Confirmation</div>;
   return (
     <>
       <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography variant='h4' align='center'>
+          <Typography variant="h4" align="center">
             Checkout
           </Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import { Elements, CardElement, ElementsConsumer } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Review from "./Review";
 
-function PaymentForm() {
-  return <div></div>;
+function PaymentForm({ checkoutToken }) {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken}></Review>
+    </>
+  );
 }
 
 export default PaymentForm;
