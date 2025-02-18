1c5f3bb14285cfa59dfd132b2948561a1613ae8e
quocpham97 <phamxuandinhquoc@gmail.com>
2025-02-18 16:14:44 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { dirs: ["/src"], ignoreDuringBuilds: true },
   output: "standalone",
-  distDir: "build", // Changes the build output directory to `build`
 };
 
 export default nextConfig;
